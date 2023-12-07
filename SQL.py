f1e58c887d3ef48c62f9098056b34b13e32087c4
RaLisa <ralisab42@gmial.com>
2023-12-06 21:46:02 -0600

    first commit

--- SQL.py
+++ SQL.py
@@ -1,5 +1,8 @@
 import mysql.connector
-
+import pandas as pd
+from pandas.io import sql
+import json
+'''
 mydb = mysql.connector.connect(
   host="localhost",
   user="yourusername",
@@ -14,5 +17,21 @@ mycursor.execute("SHOW DATABASES")
 
 for x in mycursor:
   print(x)
+
+'''
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Pa55w0rd",
+    database = "drug_conditions"
+)
+
+usdf = pd.read_json('https://api.sleeper.app/v1/league/918551365406797824/users')
+
+usdf2 = usdf[['', '']].copy()
+
+user = usdf2.copy()
+
+# print(user)
 
 
